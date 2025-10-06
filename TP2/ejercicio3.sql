0dcba1e678ba80677f022384ec76aeeda267226e
Celina Vega <142535553+CeliV@users.noreply.github.com>
2025-10-06 09:33:04 -0300

    Delete TP2/ejercicio3.sql

--- TP2/ejercicio3.sql
+++
@@ -1,5 +0,0 @@
-use gabinete_abogados;
-SELECT p.id_procurador, p.nombre, COUNT(ap.numero_expediente) AS cantidad_asuntos
-FROM Procuradores p
-LEFT JOIN Asuntos_Procuradores ap ON p.id_procurador = ap.id_procurador
-GROUP BY p.id_procurador, p.nombre;
