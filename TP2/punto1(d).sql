decd8240c025844f6647834a6bd084eb6cb4b8a2
Celina Vega <142535553+CeliV@users.noreply.github.com>
2025-10-06 09:34:54 -0300

    Delete TP2/punto1(d).sql

--- TP2/punto1(d).sql
+++
@@ -1,9 +0,0 @@
-SELECT nombre, matricula, cuota,(SELECT nombre FROM socios WHERE id_socio = barcos.id_socio) AS socionombre
-FROM barcos
-WHERE cuota > 500;
-
--- SELECT columna columna columna (columna DE SOCIOS con nombre modificado) de la tabla barco donde la cuota sea mayor a 500
--- listar los barcos que tienen una cuota mayor a 500 y sus respectivos socios
-
-
-
