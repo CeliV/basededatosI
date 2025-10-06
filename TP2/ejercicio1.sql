bcdddb1ae04477421ea20261f7997ffbad5f723e
Celina Vega <142535553+CeliV@users.noreply.github.com>
2025-10-06 09:32:43 -0300

    Delete TP2/ejercicio1.sql

--- TP2/ejercicio1.sql
+++
@@ -1,6 +0,0 @@
-USE gabinete_abogados;
-SELECT DISTINCT p.nombre, p.direccion
-FROM Procuradores p
-JOIN Asuntos_Procuradores ap ON p.id_procurador = ap.id_procurador
-JOIN Asuntos a ON ap.numero_expediente = a.numero_expediente
-WHERE a.estado = 'Abierto';
