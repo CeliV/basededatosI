62bd7232a7186c346915ed410b1ca49a77da7167
Celina Vega <142535553+CeliV@users.noreply.github.com>
2025-10-06 09:37:18 -0300

    Delete TP2/punto2(c).sql

--- TP2/punto2(c).sql
+++
@@ -1,7 +0,0 @@
-use club_nautico;
-SELECT barcos.nombre , count(*) as cant_salidas
-FROM barcos
-JOIN salidas ON barcos.matricula = salidas.matricula
-WHERE salidas.matricula = 'ABC123'
-
--- ¿Cuántas salidas ha realizado el barco con matrícula 'ABC123'?
