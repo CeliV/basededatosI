54bbd38e3739501b4ce28694e75cbec7f35ce85b
Celina Vega <142535553+CeliV@users.noreply.github.com>
2025-10-06 09:37:49 -0300

    Delete TP2/punto2(e).sql

--- TP2/punto2(e).sql
+++
@@ -1,8 +0,0 @@
-use club_nautico;
-SELECT DISTINCT barcos.nombre
-FROM salidas
-JOIN barcos ON salidas.matricula = barcos.matricula
-WHERE salidas.destino like '%Mallorca%'
-
-
--- ¿Qué barcos han salido con destino a 'Mallorca'?
