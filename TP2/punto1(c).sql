11caf5305f8c23d6e3931d6fdcda7246927d32f9
Celina Vega <142535553+CeliV@users.noreply.github.com>
2025-10-06 09:34:01 -0300

    Delete TP2/punto1(c).sql

--- TP2/punto1(c).sql
+++
@@ -1,8 +0,0 @@
-SELECT  count(*) cant_salidas
-FROM salidas
-WHERE matricula = 'ABC123';
-
-
-
--- cuantas salidas a realizado el barco con matricula abc123
--- preguntar por como hacer la cantidad de salidas si no hay una cantidad de salidas o dato repetido para usar count
