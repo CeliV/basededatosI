f5d2c0cdbd005fe7ae9277ea4a93d5e6748bfa7b
Celina Vega <142535553+CeliV@users.noreply.github.com>
2025-10-06 09:37:34 -0300

    Delete TP2/punto2(d).sql

--- TP2/punto2(d).sql
+++
@@ -1,6 +0,0 @@
-SELECT barcos.nombre , barcos.cuota , barcos.id_socio , socios.nombre AS socio_nombre
-FROM barcos 
-JOIN socios ON socios.id_socio = barcos.id_socio
-WHERE barcos.cuota > 500 
-
--- Lista los barcos que tienen una cuota mayor a 500 y sus respectivos socios.
