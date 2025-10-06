8dbf7f6a79b12f84ec1ca7cf2ea1f39a5fdc8783
Celina Vega <142535553+CeliV@users.noreply.github.com>
2025-10-06 09:36:46 -0300

    Delete TP2/punto2(b).sql

--- TP2/punto2(b).sql
+++
@@ -1,8 +0,0 @@
-SELECT barcos.nombre , barcos.cuota 
-FROM barcos
-JOIN socios ON socios.id_socio = socios.id_socio 
-WHERE socios.nombre = 'Juan Pérez'
-
-
-
--- ¿Cuáles son los nombres de los barcos y sus cuotas de aquellos barcos cuyo socio se llama 'Juan Pérez'?
